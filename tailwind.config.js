b0b4ef8fb8f2bd5f00e7cf41deea956e7b517c47
dev.tahmina <dev.tahmina@gmail.com>
2024-12-25 01:07:05 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "serif"],
+      },
+      colors: {
+        black_20: "#ffffff66",
+      },
+    },
+  },
+  plugins: [],
+};
